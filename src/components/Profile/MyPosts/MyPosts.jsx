1da13e16e23905d059375ebc80f3ead07a8913ea
poring93 <poring.m@mail.ru>
2021-04-25 23:53:37 +0300

    action-creator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { addPostActionCreator, updateNewPostActionCreator } from '../../redux/state';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
@@ -12,16 +13,13 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
  
   let addPost = () =>{
- 
-    props.dispatch({type:'ADD-POST'})
+    props.dispatch(addPostActionCreator())
     // props.dispatch('');
-  
-  
   }
 
   let onPostChange = ()=>{
     let text = newPostElement.current.value;
-    props.dispatch({type:'UPDATE-NEW-POST-TEXT',newText:text});
+    props.dispatch(updateNewPostActionCreator(text));
   }
 
     return (
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,3 +1,6 @@
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_POST = 'ADD-POST';
+
 let store = {
     _state: {
         profilePage: {
@@ -124,7 +127,7 @@ let store = {
 
 
     dispatch(action){//диспатчем заменяем все экщены
-        if (action.type === 'ADD-POST'){
+        if (action.type === ADD_POST){
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -133,15 +136,27 @@ let store = {
             this._state.profilePage.postsData.push(newPost)
             this._state.profilePage.newPostText ='';
             this._callSubscriber(this._state);
-
-
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+        } else if (action.type === UPDATE_NEW_POST_TEXT){
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
     }
 
 }
+
+export const addPostActionCreator = ()=> {
+    return{
+      type: ADD_POST
+    }
+  }
+  
+  export const updateNewPostActionCreator = (text)=> {
+    return{
+      type:UPDATE_NEW_POST_TEXT,
+      newText: text,
+    }
+  }
+
 
 export default store;
 window.store = store;
